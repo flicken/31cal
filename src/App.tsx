fa25b8fe9771b6d76c0ca734be6258e808e42637
Brian Roberts <flicken-github@flicken.net>
2022-01-28 12:26:07 +0100

    Finish login/logout button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+
+import {  GOOGLE_CLIENT_ID } from "./config";
+
 function App() {
-  return (
-    <div className="App">
-        
-    </div>
-  );
+    const [user, setUser] = useState<any>(null);
+
+    const onLogout = useCallback(
+        () => {
+            setUser(null);
+        },
+        [],
+    );
+    const onFailure = useCallback(
+        (e: any) => {
+            console.log("failure", e);
+        },
+        [],
+    );
+    const onSuccess = useCallback(
+        (e: any) => {
+            console.log("success", e);
+            setUser(e);
+        },
+        [],
+    );
+
+    if (user) {
+        const logoutText = user?.profileObj?.email ? `Logout (${user.profileObj.email})` : "Logout";
+        return <GoogleLogout
+               clientId={ GOOGLE_CLIENT_ID }
+        buttonText= {logoutText}
+        onLogoutSuccess={onLogout}
+    />;
+    } else {
+        return <GoogleLogin
+                   clientId={ `${GOOGLE_CLIENT_ID}` }
+                   buttonText="Login"
+                   onSuccess={onSuccess}
+                   onFailure={onFailure}
+                   cookiePolicy={'single_host_origin'}
+                   isSignedIn={true}
+        />
+    }
 }
 
 export default App;
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const
+  GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY || "not set",
+  GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || "not set",
+  CALENDAR_ID = process.env.REACT_APP_CALENDAR_ID || "primary"
