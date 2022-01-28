665e4428e1123a8892fae64c6850125f6200b965
Brian Roberts <flicken-github@flicken.net>
2022-01-28 14:02:52 +0100

    Use useGoogleLogin / useGoogleLogout directly
    This is to workaround error in type definitions where discovery docs are not set.

--- src/useGoogleButton.tsx
+++ src/useGoogleButton.tsx
@@ -1,11 +1,11 @@
 import React, { useCallback } from 'react';
 
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { useGoogleLogin, useGoogleLogout } from 'react-google-login';
 
 import {  GOOGLE_CLIENT_ID } from "./config";
 
 export function useGoogleButton(user: any, setUser: (arg0: any) => void) {
-    const onLogout = useCallback(
+    const onLogoutSuccess = useCallback(
         () => {
             setUser(null);
         },
@@ -25,22 +25,26 @@ export function useGoogleButton(user: any, setUser: (arg0: any) => void) {
         [],
     );
 
+    const { signIn, loaded: signInLoaded } = useGoogleLogin({
+        clientId: GOOGLE_CLIENT_ID,
+        cookiePolicy: 'single_host_origin',
+        discoveryDocs: "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+        isSignedIn: true,
+        onFailure: () => {console.log("failure");},
+        onSuccess,
+    });
+
+    const { signOut, loaded: signOutLoaded } = useGoogleLogout({
+        clientId: GOOGLE_CLIENT_ID,
+        onFailure: () => {console.log("failure");},
+        onLogoutSuccess,
+    });
+
     if (user) {
         const logoutText = user?.profileObj?.email ? `Logout (${user.profileObj.email})` : "Logout";
-        return (<GoogleLogout
-                           clientId={ `${GOOGLE_CLIENT_ID}` }
-                           buttonText= {logoutText}
-                onLogoutSuccess={onLogout}
-            />);
+        return <button onClick={signOut}>{logoutText}</button>;
     } else {
-        return (<GoogleLogin
-                           clientId={ `${GOOGLE_CLIENT_ID}` }
-                           buttonText="Login"
-                           onSuccess={onSuccess}
-                           onFailure={onFailure}
-                           cookiePolicy={'single_host_origin'}
-                           isSignedIn={true}
-            />);
+        return <button onClick={signOut}>Login</button>;
     }
 };
 
--- src/userContext.ts
+++ src/userContext.ts
@@ -2,4 +2,6 @@ import React from 'react';
 
 const userContext = React.createContext({user: null});
 
+userContext.displayName = 'userContext';
+
 export { userContext };
