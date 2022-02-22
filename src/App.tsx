7a7a06bca4dda89d5d6cad0f3c93d67bd7af8e90
Brian Roberts <flicken-github@flicken.net>
2022-02-22 17:29:41 +0100

    Add start of file import
    Improve routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,121 @@
-import React, { useState } from 'react';
-import './App.css';
-import {userContext} from './userContext';
+import React, { useState, FunctionComponent } from "react";
+import "./App.css";
+import { userContext } from "./userContext";
 
-import useClientToFetch from './google/useClientToFetch';
-import {useGoogleButton} from './useGoogleButton';
-import BulkEntry from './BulkEntry';
-import Calendars from './Calendars';
-import Events from './Events';
+import useClientToFetch from "./google/useClientToFetch";
+import { useGoogleButton } from "./useGoogleButton";
+import BulkEntry from "./BulkEntry";
+import Calendars from "./Calendars";
+import Events from "./Events";
+import ImportFile from "./ImportFile";
 
-import { Link, Switch, Route, useRoute } from "wouter";
+import type { RouteObject } from "react-router-dom";
 
-const ActiveLink = (props: {href: string, children: any}) => {
-    const [isActive] = useRoute(props.href);
-    return (
-        <Link {...props}>
-            <a className={isActive ? "active" : ""} href={props.href}>{props.children}</a>
-        </Link>
-    );
+//import Logo from "./assets/logo.svg";
+
+import {
+  BrowserRouter as Router,
+  Outlet,
+  Link,
+  useRoutes,
+  useParams,
+  useResolvedPath,
+  useMatch,
+} from "react-router-dom";
+
+const SmallLogo = (
+  <svg
+    height="1em"
+    width="1em"
+    id="Layer_1"
+    data-name="Layer 1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 122.87 122.88"
+  >
+    <title>date-31</title>
+    <path d="M81.6,4.73C81.6,2.12,84.18,0,87.37,0s5.77,2.12,5.77,4.73V25.45c0,2.61-2.58,4.73-5.77,4.73s-5.77-2.12-5.77-4.73V4.73ZM52,103.78q-4.83,0-9.36-.42a82.2,82.2,0,0,1-8.38-1.21V92.74H47.44a20.16,20.16,0,0,0,3.65-.25,3.15,3.15,0,0,0,1.78-.78,2.19,2.19,0,0,0,.48-1.51V89.06a2.67,2.67,0,0,0-.54-1.75,3.21,3.21,0,0,0-1.57-1,9.61,9.61,0,0,0-2.53-.39L38,85.43V76.38l10.19-.66A10.68,10.68,0,0,0,51.72,75a2,2,0,0,0,1.15-1.93v-.55a2.64,2.64,0,0,0-1.21-2.47,9.53,9.53,0,0,0-4.46-.72H34.89V59.91q3.91-.66,8.27-1.23a54.75,54.75,0,0,1,8.8-.46,17.18,17.18,0,0,1,6.67,1.3A9.31,9.31,0,0,1,63,63.26a12.23,12.23,0,0,1,1.5,6.37v3a13.75,13.75,0,0,1-.27,2.81,8,8,0,0,1-.87,2.35,6.43,6.43,0,0,1-1.57,1.84,7.47,7.47,0,0,1-2.36,1.27,6.31,6.31,0,0,1,2.51,1.36,8,8,0,0,1,1.75,2.2,11.32,11.32,0,0,1,1.06,2.87,15.41,15.41,0,0,1,.36,3.4v1.75q0,5.49-3.47,8.39T52,103.78Zm24.85-.37V69.33H69.76V61.84l8.45-3.62H88.59v45.19ZM29.61,4.73C29.61,2.12,32.19,0,35.38,0s5.77,2.12,5.77,4.73V25.45c0,2.61-2.58,4.73-5.77,4.73s-5.77-2.12-5.77-4.73V4.73ZM6.4,38.76H116.46V21.47a3,3,0,0,0-.86-2.07,2.92,2.92,0,0,0-2.07-.86H103a3.2,3.2,0,1,1,0-6.4h10.55a9.36,9.36,0,0,1,9.33,9.33v92.08a9.36,9.36,0,0,1-9.33,9.33H9.33A9.36,9.36,0,0,1,0,113.54V21.47a9.36,9.36,0,0,1,9.33-9.33H20.6a3.2,3.2,0,1,1,0,6.4H9.33a3,3,0,0,0-2.07.86,2.92,2.92,0,0,0-.86,2.07V38.76Zm110.07,6.41H6.4v68.37a3,3,0,0,0,.86,2.07,2.92,2.92,0,0,0,2.07.86H113.54a3,3,0,0,0,2.07-.86,2.92,2.92,0,0,0,.86-2.07V45.17Zm-66-26.63a3.2,3.2,0,0,1,0-6.4H71.91a3.2,3.2,0,1,1,0,6.4Z" />
+  </svg>
+);
+
+const routes = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        path: "/",
+        name: SmallLogo,
+        element: <div> Agenda: 31 different ways to calendar </div>,
+      },
+      {
+        path: "/bulk",
+        element: <BulkEntry />,
+      },
+      {
+        path: "/calendars",
+        element: <Calendars />,
+      },
+      {
+        path: "/events",
+        element: <Events />,
+      },
+      {
+        path: "/import",
+        element: <ImportFile />,
+      },
+    ],
+  },
+];
+
+function compareProperty(a?: string, b?: string) {
+  return a || b ? (!a ? -1 : !b ? 1 : a.localeCompare(b)) : 0;
+}
+
+const NavLink = ({ path, name }: { path: string; name?: any }) => {
+  return (
+    <li style={{ display: "inline", marginLeft: "0.25em" }}>
+      <Link to={path}>{name || path}</Link>
+    </li>
+  );
 };
 
+function Layout() {
+  const resolved = useResolvedPath("/");
+  const isHome = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <div>
+      <nav>
+        <ul style={{ listStyleType: "none", margin: 0, padding: 0 }}>
+          {routes[0]
+            .children!.sort((a, b) => compareProperty(a.path, b.path))
+            .map((r) => (
+              <NavLink {...r} />
+            ))}
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
 function App() {
-    const [user, setUser] = useState<any>(null);
-    const googleButton = useGoogleButton(user, setUser);
+  const [user, setUser] = useState<any>(null);
+  const googleButton = useGoogleButton(user, setUser);
 
-    useClientToFetch(user);
+  useClientToFetch(user);
 
-    return (
-        <userContext.Provider value={user}>
-            <ActiveLink href="/">Home</ActiveLink>
-            <div style={{float: "right", clear: "both"}}>{googleButton}</div>
-            <Switch>
-                <Route path="/bulk" component={BulkEntry} />
-                <Route path="/calendars" component={Calendars} />
-                <Route path="/events" component={Events} />
-                <Route>
-                    <nav>
-                        <ActiveLink href="/bulk">Bulk Entry</ActiveLink>
-                        <ActiveLink href="/calendars">Calendars</ActiveLink>
-                        <ActiveLink href="/events">Events</ActiveLink>
-                    </nav>
-                    Agenda: 31 different ways to calendar
-                </Route>
-            </Switch>
-        </userContext.Provider>);
+  let element = useRoutes(routes);
 
+  return (
+    <userContext.Provider value={user}>
+      <div style={{ float: "right", clear: "both" }}>{googleButton}</div>
+
+      {element}
+    </userContext.Provider>
+  );
 }
 
 export default App;
---
+++ src/ImportFile.tsx
@@ -0,0 +1,113 @@
+import React, { useState, useCallback, useMemo } from 'react';
+
+import PapaParse from 'papaparse'
+
+import {useDropzone, FileRejection, DropEvent } from 'react-dropzone';
+
+import {db} from './models/db';
+
+const parserOptions = {
+    header: true,
+    dynamicTyping: true,
+    skipEmptyLines: true,
+    transformHeader: (header: string) =>
+        header
+            .toLowerCase()
+            .replace(/\W/g, '_')
+}
+
+const baseStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column' as 'column',
+    alignItems: 'center',
+    padding: '20px',
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: '#eeeeee',
+    borderStyle: 'dashed',
+    backgroundColor: '#fafafa',
+    color: '#bdbdbd',
+    outline: 'none',
+    transition: 'border .24s ease-in-out'
+};
+
+const focusedStyle = { 
+    borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+    borderColor: '#00e676'
+};
+
+const rejectStyle = {
+    borderColor: '#ff1744'
+};
+
+function ImportFile() {
+    const [events, setEvents] = useState<Array<any>>([]);
+    const onDrop = useCallback((
+        acceptedFiles: File[],
+        fileRejections: FileRejection[],
+        e: DropEvent
+    ) => {
+        const fileEncoding = 'UTF-8';
+        const onError = (error: any) => {
+            console.log(onError);
+        };
+        acceptedFiles.forEach(file => {
+            let reader: FileReader = new FileReader()
+            reader.onload = (_event: Event) => {
+                const csvData = PapaParse.parse(
+                    reader.result as string,
+                    Object.assign(parserOptions, {
+                        error: onError,
+                        encoding: fileEncoding,
+                    }),
+                )
+                setEvents(previousEvents => [...previousEvents, ...(csvData?.data ?? [])]);
+            }
+
+            reader.readAsText(file, fileEncoding)
+        });
+    }, [setEvents]);
+
+    const {
+        getRootProps,
+        getInputProps,
+        isFocused,
+        isDragAccept,
+        isDragReject,
+        isDragActive,
+    } = useDropzone({accept: 'text/*', onDrop });
+
+    const style = useMemo(() => ({
+        ...baseStyle,
+        ...(isDragActive ? focusedStyle : {}),
+        ...(isDragAccept ? acceptStyle : {}),
+        ...(isDragReject ? rejectStyle : {})
+    }), [
+        isDragActive,
+        isDragAccept,
+        isDragReject,
+    ]);
+
+    return (<>
+          <div {...getRootProps({style})} >
+            <input {...getInputProps()} />
+        {
+            isFocused ?
+            <p>Drop the csv files here ...</p> :
+            <p>Drag 'n' drop csv files here, or click to select files</p>
+        }
+
+        </div>
+        { events && (<ul>
+            {events.map((e, i) => (<li key={i}>{JSON.stringify(e)}</li>))}
+        </ul>)
+        }
+        </>
+    )
+}
+
+export default ImportFile;
